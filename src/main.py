2ba6a946112680e501f65296667de4885d33fad8
not-Whale <rezepinn@yandex.ru>
2023-05-02 17:14:57 +0300

    Режим чтения из консоли

--- src/main.py
+++ src/main.py
@@ -2,13 +2,13 @@ import io_reader
 import file_reader
 import prototypes_parser
 
-# io_reader = io_reader.IOReader()
-file_reader = file_reader.FileReader('./tests/test1.txt')
+io_reader = io_reader.IOReader()
+# file_reader = file_reader.FileReader('./tests/test1.txt')
 
-# input1 = io_reader.get_prototypes_list()
-input2 = file_reader.get_prototypes_list()
+input1 = io_reader.get_prototypes_list()
+# input2 = file_reader.get_prototypes_list()
 
 # print(input1)
-print(input2)
+# print(input2)
 
-parser = prototypes_parser.PrototypesParser(input2, True)
+parser = prototypes_parser.PrototypesParser(input1, True)
